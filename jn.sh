6b314e771382a94e3116b3bb174a4b15d3fe4fa4
Ielton Ferreira <ieltonf@gmail.com>
2015-05-06 01:38:17 -0700

    Refinamento da regex na funcao search

--- jn.sh
+++ jn.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#########################################################################################
+###################################################################################
 #
 # NASCIMENTO    : 11 de junho de 2011
 # AUTOR     	: Ielton Ferreira Carneiro Pinto
-# DESCRICAO 	: Esse cria e atualiza a lista dos nerdcasts, tambem realiza comentarios
+# DESCRICAO 	: Esse cria e atualiza a lista dos nerdcasts, tambem realiza 
+#		  comentarios
 #
-#########################################################################################
+####################################################################################
 
 # CONFIG GLOBAIS
 WSRC="lynx --source http://jovemnerd.com.br/feed/?cat=42";
@@ -14,15 +15,11 @@ LIST="nerdcast.list";
 #DEFINE LOCAL PARA LISTA TEMPORARIA
 TLST="/tmp/nerdcast.tmp";
 
-
 # VERIFICA SE O ARQUIVO DE LISTA FOI CRIADO. 
-
 if [ ! -e nerdcast.list ];
 
 then
-
 # CASO NAO, ELE CRIA A LISTA DE TODOS OS LINKS PARA OS NERDCASTS
-
 $WSRC | sed -n -e '/<comments>.*/d' -e '/<dc:creator>/d' -e '/<category>/d' -e '/<guid.*/d' -e '/<description>.*/d' -e '/<blockquote>/,/<\/item>/d' -e 's/\+0000//g' -e 's/\t//g' -e '/<title>Nerdcast [0-9]\{1,9\}/,+25p' | sed -e :a -e 's/<[^>]*>//g' -e '/^$/d' -e '/Nerdcast [0-9]\{1,9\}.*/{x;p;x;}' -e 's/&#8211;/-/g' -e 's/&#8220;/\“/g' -e 's/&#8221;/\”/g' -e 's/&#8230;/…/g' -e 's/&#215;/×/g' -e 's/&#8216;/\‘/g' -e 's/&#038;/\&/g' -e "s/&#8217;/\'/g" > $LIST;
 
 fi
@@ -32,7 +29,6 @@ fi
 ###################################################
 
 # FUNCAO PARA ATUALIZAR LISTA
-
 update()
 {
 
@@ -46,7 +42,6 @@ LASTNC=$(diff nerdcast.list /tmp/nerdcast.tmp | sed -n '2p' | cut -c 3-);
 if [ ! -z "$LASTNC" ];
 
 then
-
 # SINCRONIZA A LISTA TEMPORARIA COM A LOCAL
 cat /tmp/nerdcast.tmp > nerdcast.list && printf "Lista atualizada! \nÚltimo EP: $LASTNC \n";
 
@@ -62,8 +57,8 @@ fi
 ################################
 search()
 {
-cat nerdcast.list | sed -n '/http:\/\/jovemnerd\.com\.br\/nerdcast\//p' | grep $1 | cut -c34- | sed 's/\///g'
-#cat nerdcast.list | grep $1
+cat nerdcast.list | sed -n -e '/http:\/\/jovemnerd\.com\.br\/nerdcast\//!d;p' | sed  's/.*\/nerdcast\///g; s/\///g; s/nerdcast-//g' | grep $1;
+
 }
 
 ###############################
@@ -72,12 +67,10 @@ cat nerdcast.list | sed -n '/http:\/\/jovemnerd\.com\.br\/nerdcast\//p' | grep $
 
 get()
 {
-#cat nerdcast.list | sed -n '/http:\/\/jovemnerd\.com\.br\/nerdcast\//p' | grep 01 | cut -c34- | sed 's/\///g'
 lynx -dump $1 | awk '/zip/{print $2}' | xargs wget -O /tmp/nerdcast_tmp.zip && unzip /tmp/nerdcast_tmp.zip -d nerdcasts/
 }
 
 case $1 in
-
 # ESSA OPCAO SOMENTE ATUALIZA A LISTA
 update)
 update
